2b801e0e4a823056994c12bd4f3713955a863ac5
Guanhua Wang <alexwgh333@gmail.com>
2016-11-18 01:54:09 -0800

    Update decisionTree.py
    add printtree and decisionnode to the decision tree

--- decisionTree.py
+++ decisionTree.py
@@ -12,3 +12,22 @@ def divideset(rows,column,value):
    set1=[row for row in rows if split_function(row)]
    set2=[row for row in rows if not split_function(row)]
    return (set1,set2)
+
+
+def printtree(tree,indent=''):
+   if tree.results!=None:
+      print(str(tree.results))
+   else:
+      print(str(tree.col)+':'+str(tree.value)+'? ')
+      print indent+'T->',
+      printtree(tree.tb,indent+'  ')
+      print indent+'F->',
+      printtree(tree.fb,indent+'  ')
+
+class decisionnode:
+   def __init__(self,col=-1,value=None,results=None,tb=None,fb=None):
+      self.col=col
+      self.value=value
+      self.results=results
+      self.tb=tb
+      self.fb=fb
